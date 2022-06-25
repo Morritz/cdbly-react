144190615851f5a77f1594d0972f45077e4ff103
Morritz <12800230+Morritz@users.noreply.github.com>
2022-06-26 00:51:57 +0200

    test for fetch message

--- src/components/CustomTable.test.tsx
+++ src/components/CustomTable.test.tsx
@@ -1,11 +1,34 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
+import GeschichteWithHistory from "geschichte/historyjs";
+import { createBrowserHistory } from "history";
+import { describe, expect, test, vi } from "vitest";
+import { CustomTable } from "./CustomTable";
 import { InfoCard } from "./InfoCard";
+
+const flushPromises = () => new Promise(setImmediate);
 
 describe("Testing custom table", () => {
   test("infocard should render with passed message", () => {
     render(<InfoCard message={"Simple message"} />);
     expect(screen.getByText(/Simple message/i)).toBeDefined();
+  });
+
+  fetch;
+  test("there should be failed to fetch message when the fetch fails", async () => {
+    global.fetch = vi.fn().mockImplementationOnce(() =>
+      Promise.resolve(
+        new Response(null, {
+          status: 500,
+          statusText: "Internal Server Error",
+        })
+      )
+    );
+    render(
+      <GeschichteWithHistory history={createBrowserHistory()}>
+        <CustomTable />
+      </GeschichteWithHistory>
+    );
+    expect(await screen.findByText(/Failed to fetch/i)).toBeDefined();
   });
 });
