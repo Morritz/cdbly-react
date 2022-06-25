a2f326f3e83ecc7c4c3dd3f720e2e1d44c1f48a2
Morritz <12800230+Morritz@users.noreply.github.com>
2022-06-25 03:30:43 +0200

    display of the items from api in table rows

--- src/components/CustomTable.tsx
+++ src/components/CustomTable.tsx
@@ -14,50 +14,62 @@ import { TableHeadCell } from "./TableHeadCell";
 import useSWR from "swr";
 import { fetcher } from "../utils/fetcher";
 import { useUrl } from "../utils/hooks/useUrl";
+import { IApiResponse } from "../interfaces/ApiResponse";
 
 const API_URL = "https://reqres.in/api/products";
 const ITEMS_PER_PAGE = 5;
 
 const CustomTable = () => {
-  const [page, setPage] = useUrl("page", "0");
-  const { data, error } = useSWR(
+  const [page, setPage] = useUrl("page", "1");
+  const { data, error } = useSWR<IApiResponse>(
     `${API_URL}?per_page=${ITEMS_PER_PAGE}&page=${page}`,
     fetcher
   );
-  console.log(data);
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableHeadCell>id</TableHeadCell>
-            <TableHeadCell>name</TableHeadCell>
-            <TableHeadCell>year</TableHeadCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell>id</TableCell>
-            <TableCell>name</TableCell>
-            <TableCell>year</TableCell>
-          </TableRow>
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              count={0}
-              page={0}
-              onPageChange={() => {
-                return;
-              }}
-              rowsPerPageOptions={[-1]}
-              rowsPerPage={5}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
+    <>
+      {data && (
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableHeadCell>id</TableHeadCell>
+                <TableHeadCell>name</TableHeadCell>
+                <TableHeadCell>year</TableHeadCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.data.map((item) => {
+                return (
+                  <TableRow
+                    sx={{
+                      backgroundColor: item.color,
+                    }}
+                  >
+                    <TableCell>{item.id}</TableCell>
+                    <TableCell>{item.name}</TableCell>
+                    <TableCell>{item.year}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  count={data.total}
+                  page={data.page - 1}
+                  onPageChange={() => {
+                    return;
+                  }}
+                  rowsPerPageOptions={[-1]}
+                  rowsPerPage={5}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      )}
+    </>
   );
 };
 
